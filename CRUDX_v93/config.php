69f2fb8ffbfbe6c7799875c0497cd71471d50709
tokesroland <tokesrolandcsete@gmail.com>
2026-02-13 12:45:16 +0100

    Add files via upload

--- CRUDX_v93/config.php
+++ CRUDX_v93/config.php
@@ -13,15 +13,21 @@
 
 //5. Jelentse az összes hibát, kivéve az E_NOTICE   ---  Report all errors except E_NOTICE 
  error_reporting(E_ALL & ~E_NOTICE);
-$BASE_URL = "/CRUDX_v93/"; // Alap URL a projekt gyökerére mutatva
+ 
+// Ha Dockerben vagyunk, a 'DB_HOST' környezeti változó 'db' lesz.
+// Ha XAMPP-ban, akkor marad 'localhost'.
+$host = getenv('DB_HOST') ?: 'localhost';
+$dbname = getenv('DB_NAME') ?: 'crudx';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: ''; // XAMPP-ban üres
 
-$host = "localhost";
-$dbname = "crudx";
-$username = "root";
-$password = "";
+// Dockerben a projekt a gyökérben van (/var/www/html), ezért a BASE_URL csak "/"
+$BASE_URL = getenv('BASE_URL') ?: '/CRUDX_v93/';
+
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Adatbázis hiba: " . $e->getMessage());
 }
+?>
